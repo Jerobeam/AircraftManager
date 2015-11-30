d6fd42d7ded62691834bf069804cdcd48857ec93
Peter <peter.truckenbrod@t-online.de>
2015-11-30 12:42:43 +0100

    test

---
+++ Airline/src/backend/Airline.java
@@ -0,0 +1,13 @@
+package backend;
+
+public class Airline {
+ public int money;
+
+public int getMoney() {
+	return money;
+}
+
+public void setMoney(int money) {
+	this.money = money;
+}
+}
---
+++ Airline/src/backend/Game.java
@@ -0,0 +1,13 @@
+package backend;
+
+public class Game {
+	private Airline airline;
+
+	public Airline getAirline() {
+		return airline;
+	}
+
+	public void setAirline(Airline airline) {
+		this.airline = airline;
+	}
+}

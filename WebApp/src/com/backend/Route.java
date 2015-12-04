d2984f1bedec89967924382fef7517b7c161eb8a
Sebastian Roehling <sebastian.roehling@sap.com>
2015-12-04 13:28:26 +0100

    created route class

---
+++ WebApp/src/com/backend/Route.java
@@ -0,0 +1,23 @@
+package com.backend;
+
+public class Route {
+
+	private String name;
+	private int distance;
+	private int demand;
+	private int costs;
+	private Airport airport1;
+	private Airport airport2;
+	
+	public Route(String name){
+		
+	}
+	
+	public void setDistance(int length){
+		
+	}
+	
+	public int getDistance(){
+		return this.distance;
+	}
+}

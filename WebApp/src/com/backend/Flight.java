440c8da259cfb19bdb878082b8088813133b1f91
Sebastian Röhling <se.roehling@gmx.de>
2015-12-13 00:50:45 +0100

    flights

--- WebApp/src/com/backend/Flight.java
+++ WebApp/src/com/backend/Flight.java
@@ -4,12 +4,17 @@ public class Flight {
 
 	private Plane plane;
 	private Route route;
+	//Duration in full hours
+	private double duration;
+	private int numberOfBookings;
 	
 	public Flight(Plane plane, Route route){
 		this.setPlane(plane);
 		this.setRoute(route);
+		//+ 1 hour for plane maintenance
+		this.duration = route.getDistance() / plane.getSpeed() + 1;
 	}
-	
+
 	public Plane getPlane() {
 		return plane;
 	}
@@ -23,5 +28,16 @@ public class Flight {
 		this.route = route;
 	}
 	
+	public double getDuration() {
+		return duration;
+	}
+
+	public int getNumberOfBookings() {
+		return numberOfBookings;
+	}
+
+	public void setNumberOfBookings(int numberOfBookings) {
+		this.numberOfBookings = numberOfBookings;
+	}
 	
 }

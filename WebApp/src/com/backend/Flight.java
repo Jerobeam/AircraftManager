de4b78950d4910c3e67017d5fedea6df5ae28de1
Sebastian Röhling <se.roehling@gmx.de>
2015-12-13 00:22:12 +0100

    flight class

--- WebApp/src/com/backend/Flight.java
+++ WebApp/src/com/backend/Flight.java
@@ -2,4 +2,26 @@ package com.backend;
 
 public class Flight {
 
+	private Plane plane;
+	private Route route;
+	
+	public Flight(Plane plane, Route route){
+		this.setPlane(plane);
+		this.setRoute(route);
+	}
+	
+	public Plane getPlane() {
+		return plane;
+	}
+	public void setPlane(Plane plane) {
+		this.plane = plane;
+	}
+	public Route getRoute() {
+		return route;
+	}
+	public void setRoute(Route route) {
+		this.route = route;
+	}
+	
+	
 }
